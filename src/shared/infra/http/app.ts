a030d639197991e9777115acd23d1c8f842470a0
bimeiras <mbmeira31@gmail.com>
2022-08-23 08:21:54 -0300

    criacao de testes de integracao

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,37 @@
+import "reflect-metadata"
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors'
+import swaggerUI from 'swagger-ui-express';
+
+
+import { createConnection } from "../typeorm/data-source";
+
+import "../../container"
+import { AppError } from '@shared/errors/AppError';
+
+import swaggerFile from '../../../swagger.json';
+import { router } from './routes';
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
+app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`
+    })
+}
+)
+
+export { app }
