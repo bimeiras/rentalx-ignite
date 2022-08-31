02487f2e243760a163cf1de872759e9f3e484b4d
bimeiras <mbmeira31@gmail.com>
2022-08-31 09:09:37 -0300

    recuperacao de senha

--- src/modules/accounts/infra/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/repositories/UsersTokensRepository.ts
@@ -38,6 +38,12 @@ class UsersTokensRepository implements IUsersTokensRepository {
         await this.repository.delete(id)
     }
 
+    async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+        const userToken = await this.repository.findOneBy({ refresh_token })
+
+        return userToken
+    }
+
 }
 
 export { UsersTokensRepository }
