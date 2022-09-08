768f8bed29c92efdb88726aaaced8c6011eb017a
bimeiras <mbmeira31@gmail.com>
2022-09-08 07:43:54 -0300

    corrigindo o refresh token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -6,8 +6,13 @@ import { verify, sign } from "jsonwebtoken"
 import { inject, injectable } from "tsyringe"
 
 interface IPayload {
-    sub: string
+    sub: string;
     email: string;
+}
+
+interface ITokenResponse {
+    token: string;
+    refresh_token: string;
 }
 
 @injectable()
@@ -19,7 +24,7 @@ class RefreshTokenUseCase {
         private dateProvider: IDateProvider
     ) {}
     
-    async execute(token: string): Promise<string> {
+    async execute(token: string): Promise<ITokenResponse> {
         const { email, sub} = verify(token, auth.secret_refresh_token) as IPayload;
 
         const user_id = sub;
@@ -48,7 +53,15 @@ class RefreshTokenUseCase {
             user_id
         })
 
-        return refresh_token
+        const newToken = sign({}, auth.secret_token, {
+            subject: user_id,
+            expiresIn: auth.expires_in_token
+        })
+
+        return {
+            refresh_token,
+            token: newToken
+        }
 
     }
 }
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -12,7 +12,6 @@ interface IPayload {
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
     const authHeader = request.headers.authorization
 
-    const usersTokensRepository = new UsersTokensRepository()
 
     if (!authHeader) {
         throw new AppError("Missing token!", 401)
@@ -21,15 +20,7 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     const [, token] = authHeader.split(" ")
 
     try { 
-        const { sub: user_id } = verify(token, auth.secret_refresh_token) as IPayload
-
-        const usersRepository = new UsersRepository()
-
-        const user = await usersTokensRepository.findByUserIdAndRefreshToken(user_id, token)
-
-        if(!user) {
-            throw new AppError("User does not exists!", 401)
-        }
+        const { sub: user_id } = verify(token, auth.secret_token) as IPayload
 
         request.user = { 
             id: user_id
